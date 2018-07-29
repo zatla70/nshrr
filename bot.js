1b9ef66e5d2340db2172f4fb67239119bb53ebfd
7senae <41513481+7senae@users.noreply.github.com>
2018-07-29 15:45:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,19 +9,20 @@ console.log("Welcome Again  by Fox!");
 Client.on("guildMemberRemove", member => {
   member.send(`***:small_blue_diamond: - السلام عليكم ورحمة الله وبركاته ..
 
-:fish_cake:  https://discord.gg/azSj5AV
+:fish_cake:  https://discord.gg/XSURQ5
 يعلن سيرفرنا عن قيام مسابقه والشروط جد سهله و مضمونه .
 الجوائز :
-المركز الاول:  40$ + 500.000 كريديت
-المركز الثاني:  20$ + 240.000 كريديت
-المركز الثالت:  10$ + 100.000 كريديت
+المركز الاول:  400$ + 500.000 كريديت
+المركز الثاني:  200$ + 240.000 كريديت
+المركز الثالت:  100$ + 100.000 كريديت
 @here 
+و 1000 حساب فل اكسسس اتمنه دخلون بس نوصل 1000 عضو نعلن عن الفاىز
 الشروط ترسل صوره ل @@!       MDMRHM  4Ӄ 
 و تنشر السيرفو بكل مكان .
 طريقه تخليك تفوز :
 تقل لاخوانك يصوتولك عرقمك بالمسابقه
 حض موفق للجميع
-https://discord.gg/azSj5AV
+https://discord.gg/XSURQ5
 [${member}]**`);
 
   });
@@ -30,19 +31,20 @@ https://discord.gg/azSj5AV
   Client.on('guildMemberAdd', member => {
   member.send(`***:small_blue_diamond: - السلام عليكم ورحمة الله وبركاته ..
 
-:fish_cake:  https://discord.gg/azSj5AV
+:fish_cake:  https://discord.gg/XSURQ5
 يعلن سيرفرنا عن قيام مسابقه والشروط جد سهله و مضمونه .
 الجوائز :
-المركز الاول:  40$ + 500.000 كريديت
-المركز الثاني:  20$ + 240.000 كريديت
-المركز الثالت:  10$ + 100.000 كريديت
+المركز الاول:  400$ + 500.000 كريديت
+المركز الثاني:  200$ + 240.000 كريديت
+المركز الثالت:  100$ + 100.000 كريديت
 @here 
+و 1000 حساب فل اكسسس اتمنه دخلون بس نوصل 1000 عضو نعلن عن الفاىز
 الشروط ترسل صوره ل @@!       MDMRHM  4Ӄ 
 و تنشر السيرفو بكل مكان .
 طريقه تخليك تفوز :
 تقل لاخوانك يصوتولك عرقمك بالمسابقه
 حض موفق للجميع
-https://discord.gg/azSj5AV
+https://discord.gg/XSURQ5
 [${member}]**`);
 
 
@@ -50,4 +52,4 @@ https://discord.gg/azSj5AV
 
 
 
-Client.login("NDcyNzcxODUwODYzMjQ3MzYx.Dj4OyQ.Hm7avdVwqmwARnjvA3Hc6m_U1NY");
+Client.login("NDczMTAzMTU1MzUwMDc3NDUx.Dj9D8A.oJw57H3Ev2UL9qo2fHa7jrxZUG0");
