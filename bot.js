ec7e8a814e77d9a5185dce388fc50087badfa7fd
zatla70 <44783807+zatla70@users.noreply.github.com>
2018-11-07 22:16:54 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,44 +7,18 @@ console.log("Welcome Again  by Fox!");
 
 
 Client.on("guildMemberRemove", member => {
-  member.send(`***:small_blue_diamond: - السلام عليكم ورحمة الله وبركاته ..
+  member.send(`
 
-:fish_cake:  https://discord.gg/XSURQ5
-يعلن سيرفرنا عن قيام مسابقه والشروط جد سهله و مضمونه .
-الجوائز :
-المركز الاول:  400$ + 500.000 كريديت
-المركز الثاني:  200$ + 240.000 كريديت
-المركز الثالت:  100$ + 100.000 كريديت
-@here 
-و 1000 حساب فل اكسسس اتمنه دخلون بس نوصل 1000 عضو نعلن عن الفاىز
-الشروط ترسل صوره ل @@!       MDMRHM  4Ӄ 
-و تنشر السيرفو بكل مكان .
-طريقه تخليك تفوز :
-تقل لاخوانك يصوتولك عرقمك بالمسابقه
-حض موفق للجميع
-https://discord.gg/XSURQ5
+
 [${member}]**`);
 
   });
  
 
   Client.on('guildMemberAdd', member => {
-  member.send(`***:small_blue_diamond: - السلام عليكم ورحمة الله وبركاته ..
+  member.send(`
 
-:fish_cake:  https://discord.gg/XSURQ5
-يعلن سيرفرنا عن قيام مسابقه والشروط جد سهله و مضمونه .
-الجوائز :
-المركز الاول:  400$ + 500.000 كريديت
-المركز الثاني:  200$ + 240.000 كريديت
-المركز الثالت:  100$ + 100.000 كريديت
-@here 
-و 1000 حساب فل اكسسس اتمنه دخلون بس نوصل 1000 عضو نعلن عن الفاىز
-الشروط ترسل صوره ل @@!       MDMRHM  4Ӄ 
-و تنشر السيرفو بكل مكان .
-طريقه تخليك تفوز :
-تقل لاخوانك يصوتولك عرقمك بالمسابقه
-حض موفق للجميع
-https://discord.gg/XSURQ5
+
 [${member}]**`);
 
 
@@ -52,4 +26,4 @@ https://discord.gg/XSURQ5
 
 
 
-Client.login("NDczMTAzMTU1MzUwMDc3NDUx.Dj9D8A.oJw57H3Ev2UL9qo2fHa7jrxZUG0");
+Client.login(process.env.BOT_TOKEN));
